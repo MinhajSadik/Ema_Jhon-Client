1666ebc80ccda1f9fb47da17a63317a472b4a349
Minhaj Sadik <MinhajoSadik@gmail.com>
2021-03-26 19:57:20 +0600

    Two Component Added

--- src/App.js
+++ src/App.js
@@ -4,6 +4,8 @@ import Header from './Componenet/Header/Header';
 import Shop from './Componenet/Shop/Shop';
 import Review from './Componenet/Review/Review';
 import Inventory from './Componenet/Inventory/Inventory';
+import Login from './Componenet/Login/Login';
+import Shipment from './Componenet/Shipment/Shipment';
 import NotFound from './Componenet/NotFound/NotFound';
 import ProductDetail from './Componenet/ProductDetail/ProductDetail';
 
@@ -19,15 +21,26 @@ function App() {
     <Header></Header>
       <Router>
         <Switch>
+
           <Route path="/shop">
             <Shop/>
           </Route>
+          
           <Route path="/review">
             <Review/>
           </Route>
+
           <Route path="/inventory">
             <Inventory/>
           </Route>
+
+          <Route path="/login">
+            <Login/>
+          </Route>
+          <Route path="/shipment">
+            <Shipment/>
+          </Route>
+
           <Route exact path="/">
             <Shop/>
           </Route>
@@ -39,7 +52,7 @@ function App() {
           <Route path="*">
             <NotFound/>
         </Route>
-          
+  
       </Switch>
     </Router>
   </div>
