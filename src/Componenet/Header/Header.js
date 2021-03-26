bf0ae8da49a108be44729b79744131514a7c183d
Minhaj Sadik <MinhajoSadik@gmail.com>
2021-03-27 02:15:30 +0600

    sign out button implement

--- src/Componenet/Header/Header.js
+++ src/Componenet/Header/Header.js
@@ -1,8 +1,10 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { Link } from 'react-router-dom';
+import { UserContext } from '../../App';
 import logo from '../../images/logo.png';
 import './Header.css';
 const Header = () => {
+    const [loggedInUser, setLoggedInUser] = useContext(UserContext)
     return (
         <div className="header">
             <img src={logo} alt=""/>
@@ -10,6 +12,7 @@ const Header = () => {
                 <Link to="/shop">Shop</Link>
                 <Link to="/review">Review</Link>
                 <Link to="/inventory">Manage Inventory</Link>
+                <button style={{backgroundColor: 'green'}} onClick={() => setLoggedInUser({})}>Sign Out</button>
             </nav>
         </div>
     );
