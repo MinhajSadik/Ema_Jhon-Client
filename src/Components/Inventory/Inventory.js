4a3e9a855e369858593c6cf2553ba40666fc67bf
MinhajSadik <MinhajoSadik@gmail.com>
2021-12-15 23:01:51 +0600

    [ADD] frontend part working on

--- src/Components/Inventory/Inventory.js
+++ src/Components/Inventory/Inventory.js
@@ -1,11 +1,22 @@
-import React from 'react';
+import React from "react";
+import fakeData from "../../fakeData";
 
 const Inventory = () => {
-    return (
-        <div>
-            <h1>Inventory is Cumming Soon</h1>
-        </div>
-    );
+  //addProduct endpoint
+  const handleAddProduct = () => {
+    fetch("http://localhost:3000/addProduct", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(fakeData),
+    });
+  };
+  return (
+    <div>
+      <button onClick={handleAddProduct}>Add Product</button>
+    </div>
+  );
 };
 
 export default Inventory;
--- src/Components/Login/Login.js
+++ src/Components/Login/Login.js
@@ -1,134 +1,164 @@
-import { useContext, useState } from 'react';
-import { UserContext } from "../../App";
+import { useContext, useState } from "react";
 import { useHistory, useLocation } from "react-router";
-import { handleGoogleSignIn, handleFbSignIn, handleSignOut, initializeLoginFramwork, createUserWithEmailAndPassword, signInWithEmailAndPassword } from './LoginManager';
+import { UserContext } from "../../App";
+import {
+  createUserWithEmailAndPassword,
+  handleFbSignIn,
+  handleGoogleSignIn,
+  handleSignOut,
+  initializeLoginFramwork,
+  signInWithEmailAndPassword,
+} from "./LoginManager";
 
 // if (!firebase.apps.length) {
-    // firebase.initializeApp(firebaseConfig)
+// firebase.initializeApp(firebaseConfig)
 // }
 
 const Login = () => {
-    const [newUser, setNewUser] = useState(false);
-    const [user, setUser] = useState({
-        newUser: false,
-        isSignedIn: false,
-        name: '',
-        email: '',
-        photo: '',
-        error: '',
-        success: false
-    })
-    initializeLoginFramwork()
+  const [newUser, setNewUser] = useState(false);
+  const [user, setUser] = useState({
+    newUser: false,
+    isSignedIn: false,
+    name: "",
+    email: "",
+    photo: "",
+    error: "",
+    success: false,
+  });
+  initializeLoginFramwork();
 
-    // eslint-disable-next-line no-unused-vars
-    const [loggedInUser, setLoggedInUser] = useContext(UserContext);
-    const history = useHistory();
-    const location = useLocation();
-    let { from } = location.state || { from: { pathname: "/" } };
+  // eslint-disable-next-line no-unused-vars
+  const [loggedInUser, setLoggedInUser] = useContext(UserContext);
+  const history = useHistory();
+  const location = useLocation();
+  let { from } = location.state || { from: { pathname: "/" } };
 
-    const googleSignIn = () => {
-        handleGoogleSignIn()
-            .then(res => {
-                handleResponse(res, true);
-        })
+  const googleSignIn = () => {
+    handleGoogleSignIn().then((res) => {
+      handleResponse(res, true);
+    });
+  };
+  const fbSignIn = () => {
+    handleFbSignIn().then((res) => {
+      handleResponse(res, true);
+    });
+  };
+  const signOut = () => {
+    handleSignOut().then((res) => {
+      handleResponse(res, false);
+    });
+  };
+
+  const handleResponse = (res, redicrect) => {
+    setUser(res);
+    setLoggedInUser(res);
+    if (redicrect) {
+      history.replace(from);
     }
-    const fbSignIn = () => {
-        handleFbSignIn()
-            .then(res => {
-                handleResponse(res, true);
-        })
+  };
+
+  const handleBlur = (event) => {
+    let isFieldValid;
+    if (event.target.name === "email") {
+      isFieldValid = /\S+@\S+\.\S+/.test(event.target.value);
     }
-    const signOut = () => {
-        handleSignOut()
-            .then(res => {
-                handleResponse(res, false);
-        })
+    if (event.target.name === "password") {
+      const isPasswordValid = event.target.value.length > 6;
+      const passwordHasNumber = /\d{3}/.test(event.target.value);
+      isFieldValid = isPasswordValid && passwordHasNumber; // javascript regexp code chack out
     }
-    
-    const handleResponse = (res, redicrect) => {
-        setUser(res);
-        setLoggedInUser(res);
-        if (redicrect) {
-            history.replace(from);
-        }
+    if (isFieldValid) {
+      const newUserInfo = { ...user };
+      newUserInfo[event.target.name] = event.target.value;
+      setUser(newUserInfo);
     }
-
-    const handleBlur = (event) => {
-        let isFieldValid;
-        if (event.target.name === 'email') {
-            isFieldValid = /\S+@\S+\.\S+/.test(event.target.value)
+  };
+  const handleSubmit = (e) => {
+    if (newUser && user.email && user.password) {
+      createUserWithEmailAndPassword(user.name, user.email, user.password).then(
+        (res) => {
+          handleResponse(res, true);
         }
-        if (event.target.name === 'password') {
-            const isPasswordValid = event.target.value.length > 6;
-            const passwordHasNumber = /\d{3}/.test(event.target.value);
-            isFieldValid = isPasswordValid && passwordHasNumber  // javascript regexp code chack out
-        }
-        if (isFieldValid) {
-            const newUserInfo = { ...user };
-            newUserInfo[event.target.name] = event.target.value;
-            setUser(newUserInfo)
-        }
+      );
     }
-    const handleSubmit = (e) => {
-        if (newUser && user.email && user.password) {
-            createUserWithEmailAndPassword(user.name, user.email, user.password)
-                .then(res => {
-                    handleResponse(res, true);
-            })
-        }
-        if (!newUser && user.email && user.password) {
-            signInWithEmailAndPassword(user.email, user.password)
-                .then(res => {
-                    handleResponse(res, true);
-            })
-        }
-        e.preventDefault();
+    if (!newUser && user.email && user.password) {
+      signInWithEmailAndPassword(user.email, user.password).then((res) => {
+        handleResponse(res, true);
+      });
     }
+    e.preventDefault();
+  };
 
-   
+  return (
+    <div className="App">
+      {user.isSignedIn ? (
+        <button onClick={signOut}>Sign Out</button>
+      ) : (
+        <button onClick={googleSignIn}>Google Sign In</button>
+      )}
+      <br />
+      <button onClick={fbSignIn}>Facebook Sign In</button>
 
-    return (
-        <div className="App">
-            {
-                user.isSignedIn ?
-                    <button onClick={signOut}>Sign Out</button>
-                    :
-                    <button onClick={googleSignIn}>Sign In</button>
-            }
-            <br />
-            <button onClick={fbSignIn}>Sign In With Facebook</button>
+      {user.isSignedIn && (
+        <div>
+          <p>Welcome! {user.name}</p>
+          <p>Your Email: {user.email}</p>
+          <img src={user.photo} alt="" />
+        </div>
+      )}
 
-            {
-                user.isSignedIn &&
-                <div>
-                    <p>Welcome! {user.name}</p>
-                    <p>Your Email: {user.email}</p>
-                    <img src={user.photo} alt="" />
-                </div>
-            }
+      <h1>Our Own Authentication</h1>
+      <input
+        type="checkbox"
+        onChange={() => setNewUser(!newUser)}
+        name="newUser"
+        id=""
+      />
+      <label htmlFor="newUser">New User Sign Up</label>
 
-            <h1>Our Own Authentication</h1>
-            <input type="checkbox" onChange={() => setNewUser(!newUser)} name="newUser" id="" />
-            <label htmlFor="newUser">New User Sign Up</label>
-
-            {/* <p>Name: {user.name}</p>
+      {/* <p>Name: {user.name}</p>
             <p>Email: {user.email}</p>
             <p>Password: {user.password}</p> */}
 
-            <form onSubmit={handleSubmit}>
-                {newUser && <input onBlur={handleBlur} placeholder="Enter Your Name:" name="name" type="text" required />}
-                <br />
-                <input onBlur={handleBlur} type="text" name="email" placeholder="Enter Your Email" required />
-                <br />
-                <input onBlur={handleBlur} type="password" name="password" placeholder="Enter Your Password" id="" required />
-                <br />
-                <input type="submit" value={newUser ? 'Sign up' : 'Sign in'} />
-            </form>
-            <p style={{ color: 'red' }}> {user.error} </p>
+      <form onSubmit={handleSubmit}>
+        {newUser && (
+          <input
+            onBlur={handleBlur}
+            placeholder="Enter Your Name:"
+            name="name"
+            type="text"
+            required
+          />
+        )}
+        <br />
+        <input
+          onBlur={handleBlur}
+          type="text"
+          name="email"
+          placeholder="Enter Your Email"
+          required
+        />
+        <br />
+        <input
+          onBlur={handleBlur}
+          type="password"
+          name="password"
+          placeholder="Enter Your Password"
+          id=""
+          required
+        />
+        <br />
+        <input type="submit" value={newUser ? "Sign up" : "Sign in"} />
+      </form>
+      <p style={{ color: "red" }}> {user.error} </p>
 
-            {user.success && <p style={{ color: 'green' }}>{newUser ? 'Created' : 'Logged In'} Successfully</p>}
-        </div>
-    );
-}
+      {user.success && (
+        <p style={{ color: "green" }}>
+          {newUser ? "Created" : "Logged In"} Successfully
+        </p>
+      )}
+    </div>
+  );
+};
 
 export default Login;
